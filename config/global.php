21a3bfb504ee94edbf152417c364f91335108eb8
Yesid Perea <yesid.perea@outlook.com>
2019-09-16 21:32:16 -0500

    Changing HOST and SERVICE_URL globals for routing from taskmanager subfolder.

--- config/global.php
+++ config/global.php
@@ -3,10 +3,10 @@
     define("CONTROLLER", "Tasks");
     define("ACTION", "Index");
     define('ROOT', $_SERVER['DOCUMENT_ROOT']);
-    define('HOST', '');//LOCAL -> Change before deploying
+    define('HOST', 'taskmanager/');//LOCAL -> Change before deploying
     define('CORE', 'core/');
     define('CONTROLLERS', 'controllers/');
     define('MODELS', 'models/');
     define('VIEWS', 'views/');
-    define('SERVICE_URL', 'http://localhost/services/taskservice.php'); //LOCAL -> Change before deploying
+    define('SERVICE_URL', 'http://localhost/taskmanager/services/taskservice.php'); //LOCAL -> Change before deploying
 ?>
